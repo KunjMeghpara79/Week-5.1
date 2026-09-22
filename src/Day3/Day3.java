abcef00d7c1da6e1fbe18573f5f0235e05c5d844
Kunj Meghapara <kunjmeghapara777@gmail.com>
2026-09-22 12:14:04 +0530

    ..

--- src/Day3/Day3.java
+++ src/Day3/Day3.java
@@ -1,7 +1,7 @@
 package Day3;
 
 public class Day3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThirdPartyShipping thirdPartyShipping = new ThirdPartyShipping();
         ShippingService shippingService = new ShippingAdapter(thirdPartyShipping);
         shippingService.ship("12345");
